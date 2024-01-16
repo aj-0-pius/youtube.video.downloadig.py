6e6f0346c3a277429a668cd90e544cda837c19a2
Minhajul Abadin Pius <96046596+aj-0-pius@users.noreply.github.com>
2024-01-17 03:29:04 +0600

    Update youtube_video_download.py
    Now user Can download in which resolution user want

--- youtube_video_download.py
+++ youtube_video_download.py
@@ -1,18 +1,41 @@
 from pytube import YouTube
 
-#video link
+def seconds_to_minutes(seconds):
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    return minutes, remaining_seconds
+
+def bytes_to_megabytes(bytes_size):
+    return bytes_size / (1024 * 1024)
+
+# YouTube video link
 link = input("YouTube video Link: ")
 yt = YouTube(link)
-#Shows details
-print("Title: ",yt.title)
-print("Number of views: ",yt.views)
-print("Length of video: ",yt.length)
-print("Rating of video: ",yt.rating)
-#Getting  highest resolution 
-ys = yt.streams.get_highest_resolution()
-#Download Path
+
+# Show video details
+print("Title: ", yt.title)
+print("Number of views: ", yt.views)
+
+# Convert video length to minutes
+video_length_minutes, video_length_seconds = seconds_to_minutes(yt.length)
+print(f"Length of video: {video_length_minutes} minutes and {video_length_seconds} seconds")
+
+print("----aj_0_pius----")
+# Input download path from the user
 download_path = '/storage/emulated/0/storag'
-#Starting download
+
+# Display available video resolutions
+print("\nAvailable video resolutions:")
+for i, stream in enumerate(yt.streams.filter(progressive=True).order_by('resolution'), start=1):
+    print(f"{i}. Resolution: {stream.resolution}, Size: {bytes_to_megabytes(stream.filesize):.2f} MB")
+
+# Get the desired resolution input from the user
+desired_resolution_number = int(input("Enter resolution: "))
+
+# Find desired resolution
+ys = list(yt.streams.filter(progressive=True).order_by('resolution'))[desired_resolution_number - 1]
+
+# Starting download
 print("Downloading...")
-ys.download()
+ys.download(download_path)
 print("Download completed!! \naj_0_pius")
